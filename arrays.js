135cd76d35646c88117a39a4f64fe26b90e2af59
craig sperlazza <craig.sperlazza@gmail.com>
2018-01-02 19:36:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,7 +25,7 @@ function accessElementInArray (array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray (array) {
-  return array.pop
+  return array.shift
 }
 
 function removeElementFromBeginningOfArray (array) {
